051454bd2ac7e4dc76789cb3cc5ddc02467adf7f
rayhogan <rayhogan@gmail.com>
2024-11-30 20:16:51 +0000

    Initial commit

---
+++ AoC2024/Day1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AoC2024
+{
+    public static class Day1
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AoC2024/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AoC2024;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Day1.Run();
+    }
+}
